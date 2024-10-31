19a700b580b18e6d5e1b3b098a4e66f1d6851a86
Atharva Kote <atharvkote3@gmail.com>
2024-10-31 19:07:36 +0530

    commit

--- frontend/main-module/index.php
+++ frontend/main-module/index.php
@@ -18,31 +18,12 @@
  </head>
 
  <body>
-
-     <header>
-         <img src="../../images/FigmaLogo.svg" height="100px" width="200px">
-         <div class="logo">
-             <h1>Zero Hunger</h1>
-             <p><b>Nourishing Lives, Creating Smiles!</b></p>
-         </div>
-         <div class="login-logo">
-             <a href="../user-module/profilePage.php">
-                 <img src="../../images/Person-Logo.png" height="50px" width="50px">
-             </a>
-         </div>
-     </header>
-
-     <nav class="navbar">
-         <ul>
-             <li><a href="#">Home</a></li>
-             <li><a href="#">Latest</a></li>
-             <li><a href="#">About Us</a></li>
-         </ul>
-     </nav>
-
+     <?php
+         include '../assets/navbar.html';  // import navbar as a componenet 
+     ?>
      <div class="main-container">
          <div class="left">
-             <h1>Zero Hunger</h1>
+             <h1>Zero Hunger - Be the Change</h1>
              <div class="content">
                  <div class="left-content">
                      <p>The Zero Hunger project is a website designed to help reduce food waste and fight hunger. It connects people who have extra food with those who need it.</p>
@@ -62,10 +43,10 @@
                      रक्षिणी संसारस्य सर्वस्य कुरु कुशलम्। </p>
              </div>
              <div class="button-group">
-                 <a href="#"><button><b>Need Food</b></button></a>
-                 <a href="#"><button><b>Donate Food</b></button></a>
-                 <a href="#"><button><b>Report Wastage</b></button></a>
-                 <a href="#"><button><b>Volunteer</b></button></a>
+                 <a href="../functionality-module/needFood.php"><button><b>Need Food</b></button></a>
+                 <a href="../functionality-module/donateFood.php"><button><b>Donate Food</b></button></a>
+                 <a href="../functionality-module/volunteer-module/volunteer.html"><button><b>Volunteer</b></button></a>
+                 <a href="../functionality-module/sponser-module/sponser.html"><button><b>Sponser Us</b></button></a>
              </div>
          </div>
      </div>
---
+++ frontend/volunteer-module/volunteer.php
@@ -0,0 +1,76 @@
+<html>
+
+<head>
+  <title>Zero Hunger - Volunteer Registration</title>
+  <link href="volunteer-style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <?php
+  include '../assets/navbar.html';  // import navbar as a componenet 
+  ?>
+  <h2 class="heading">Volunteer Registration</h2>
+  <div class="container">
+    <div class="form-container">
+      <div class="form-group">
+        <label for="mobile-number">Enter Your Mobile Number</label>
+        <input placeholder="Enter Your Personal Contact Mobile Number" type="text" />
+      </div>
+
+      <div class="form-group date-icon">
+        <label for="birth-date">Birth Date</label>
+        <input id="birth-date" type="date" placeholder="Your Birth Date" />
+      </div>
+
+      <div class="form-group">
+        <label for="address">Residential Address</label>
+        <textarea id="address" placeholder="Enter Your Address"></textarea>
+      </div>
+
+      <h2 class="subheading">Volunteer Role</h2>
+
+      <div class="volunteer-role">
+        <div class="checkboxx">
+          <div class="check">
+          <input id="serving-agent" type="checkbox" />
+          <label for="serving-agent">Serving Agent</label>
+          </div>
+          <div class="check">
+          <input id="food-collector" type="checkbox" />
+          <label for="food-collector">Food Collector</label>
+          </div>
+          <div class="check">
+          <input id="quality-manager" type="checkbox" />
+          <label for="quality-manager">Quality Manager</label>
+          </div>
+          <div class="check">
+          <input id="developer" type="checkbox" />
+          <label for="developer">Developer</label>
+          </div>
+        </div>
+        <div class="role-guide">
+          <a href="#"><button class="role-guide">Role Guide</button></a>
+          <a href="#"><button class="role-guide">Role Guide</button></a>
+          <a href="#"><button class="role-guide">Role Guide</button></a>
+          <a href="#"><button class="role-guide">Role Guide</button></a>
+        </div>
+      </div>
+
+    </div>
+      <div class="info-container">
+        <img  height="600px" width="500px" src="../../images/Red-Heart-Front-Logo.png"/>
+        <h2 >Join Us ! As a Volunteer</h2>
+        <div class="submit-warning">
+          <h3>Instructions</h3>
+          <ol>
+            <li>Multiple Role for one volunter is Possible, In this Individual should be able to perform all the roles</li>
+            <li>Serious action will be taken against Fake Submission,</li>
+            <li>After Selection as volunteer, Causal Behevoir will not be entertained</li>
+          </ol>
+        </div>
+        <button class="submit-button-new">Submit</button>
+      </div>
+  </div>
+</body>
+
+</html>
